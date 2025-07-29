877b852bad368f741bf07782940d152a1e94c81a
Alexandre Macabies <web+oss@zopieux.com>
2025-07-29 19:53:16 +0200

    Opus all the things. Add The Ground.

--- src/Songs.ts
+++ src/Songs.ts
@@ -3,10 +3,10 @@ export interface Song {
     urls: string[];
 }
 
-function url(stem: string, ext: string = "mp3"): string[] {
+function url(stem: string, ext: string = "opus"): string[] {
     return [`/songs/${stem}.${ext}`]
 }
-function urls(stem: string, ext: string = "mp3"): string[] {
+function urls(stem: string, ext: string = "opus"): string[] {
     return ["music", "vocals"].map(s => `/songs/${stem}.${s}.${ext}`)
 }
 
@@ -14,8 +14,9 @@ export default function getAvailableSongs(): Song[] {
     return [
         { name: "Ave Verum Corpus, K. 618", urls: urls("ave-verum-corpus") },
         { name: "Cantique de Jean Racine", urls: urls("cantique-jean-racine") },
-        { name: "Va pensiero", urls: urls("va-pensiero", "opus") },
+        { name: "Va pensiero", urls: urls("va-pensiero") },
         { name: "Va pensiero", urls: url("va-pensiero") },
-        { name: "Seasons of Love", urls: urls("seasons", "opus") },
+        { name: "Seasons of Love", urls: urls("seasons") },
+        { name: "The Ground", urls: urls("the-ground") },
     ].sort((a, b) => a.name.localeCompare(b.name, "en", { sensitivity: "base" }))
 }
